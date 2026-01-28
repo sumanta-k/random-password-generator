12405ede3adb615b76247cec46e8dd98366e3e25
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-01-28 08:08:38 +0530

    Add logic provider js file

---
+++ script.js
@@ -0,0 +1,113 @@
+const characters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "~",
+    "`",
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "(",
+    ")",
+    "_",
+    "-",
+    "+",
+    "=",
+    "{",
+    "[",
+    "}",
+    "]",
+    ",",
+    "|",
+    ":",
+    ";",
+    "<",
+    ">",
+    ".",
+    "?",
+    "/",
+];
+
+let firstOption = document.querySelector("#show-passwd-1");
+let secondOption = document.querySelector("#show-passwd-2");
+
+function generateRandomPassword() {
+    let randomPasswd = "";
+    for (let i = 1; i < 11; i++) {
+        randomPasswd +=
+            characters[Math.floor(Math.random() * characters.length)];
+    }
+    return randomPasswd;
+}
+
+function showPasswd1() {
+    firstOption.textContent = generateRandomPassword();
+}
+
+function showPasswd2() {
+    secondOption.textContent = generateRandomPassword();
+}
